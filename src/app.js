92df185426336283f276cb07862e2c08aff2df46
axundra <axundra@hotmail.com>
2024-07-19 21:29:13 -0300

    las listas ahora estan en JSONs

--- src/app.js
+++ src/app.js
@@ -9,43 +9,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/api/products', ProductsRoute)
 app.use('/api/carts', CartRoute)
 
-
-global.productos = [{
-    id: 0,
-    title: "",
-    description: "",
-    code: "",
-    price: 0,
-    status: true,
-    stock: 0,
-    category: "",
-    propiedad: "HardCodeado"
-}, {
-    id: 1,
-    title: "",
-    description: "",
-    code: "",
-    price: 0,
-    status: false,
-    stock: 0,
-    category: "",
-    propiedad: "HardCodeado"
-}];
-
-global.carrito = [{
-    id: 0,
-    products: [
-        { productID: 5, cantidad: 4, propiedad: "HardCodeado" },
-        { productID: 2, cantidad: 5, propiedad: "HardCodeado" }
-    ]
-},{
-    id: 1,
-    products: [
-        { productID: 0, cantidad: 3, propiedad: "HardCodeado" },
-        { productID: 4, cantidad: 2, propiedad: "HardCodeado" }
-    ]
-}]
-
 app.listen(8080, () => {
     console.log('mondongo');
 })
