42e1f1647d2803890112fe62d03d59b3e963c2e6
axundra <axundra@hotmail.com>
2024-07-19 22:06:04 -0300

    añadido fileSystem

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -1,21 +1,41 @@
-import { Router } from "express";
+import { Router } from "express"
+import fs from 'fs'
 
-const router = Router()
+const router = Router(),
+    cart = JSON.parse(fs.readFileSync('database/cart.json', 'utf-8'));
 
-router.post("/", (req, res) => {
-    const ID = Date.now(),      //genera ID en base a la fecha
-        body = req.body;
-    carrito.push([{ id: ID, products: [body] }])
-    res.json(carrito)
+
+//no lo pedía la consigna pero mepareció bien ponerlo
+router.get('/', (req, res) => {
+    res.json(cart)
 })
 
+
+
+
+//crear carrito nuevo
+router.post("/", (req, res) => {
+    const ID = Date.now()      //genera ID en base a la fecha
+
+    cart.push({ id: ID, products: [] })
+
+    fs.writeFileSync('database/cart.json', JSON.stringify(cart))
+    res.json(cart)
+})
+
+
+
+//buscar carrito por ID
 router.get("/:cartID", (req, res) => {
-    const find = carrito.find((el => el.id == Number(req.params.cartID)))
+    const find = cart.find((el => el.id == Number(req.params.cartID)))
     res.json(find.products)
 })
 
+
+
+//añadir producto al carrito específico o aumentar su cantidad
 router.post("/:cartID/product/:productID", (req, res) => {
-    const Cfind = carrito.find(el => el.id == Number(req.params.cartID))   //busca el ID de compra
+    const Cfind = cart.find(el => el.id == Number(req.params.cartID))   //busca el ID de compra
     if (Cfind) {//chekea si existe la orden de compra
         const prodID = Number(req.params.productID),
             findd = Cfind.products.find(el2 => el2.productID == prodID)         //busca el producto dentro de la lista filtrada por ID para después aumentar su cantidad
@@ -30,9 +50,10 @@ router.post("/:cartID/product/:productID", (req, res) => {
             })
         }
 
-        res.json({ carrito })
-    }else{//si el ID de compra no existe
-        res.status(500).json({"Error": "ID de transaccion inexistente"})
+        fs.writeFileSync('database/cart.json', JSON.stringify(cart))
+        res.json({ cart })
+    } else {//si el ID de compra no existe
+        res.status(500).json({ "Error": "ID de transaccion inexistente" })
     }
 })
 
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,34 +1,50 @@
 import { Router } from "express";
+import fs from 'fs'
 
 const router = Router()
+let productos = JSON.parse(fs.readFileSync('database/products.json', 'utf-8'));
 
+
+//muestra todos los productos
 router.get("/", (req, res) => {
     res.json({ ...productos })
 })
 
+
+//muestra un solo producto por ID
 router.get("/:productID", (req, res) => {
     const find = productos.find((el => el.id == Number(req.params.productID)))
     res.json(find)
 })
 
+
+//agrega productos con ID incremental en base al N° de ID más grande
 router.post("/", (req, res) => {
-    const ID = Number(productos.reduce((max, el) => (el.id > max ? el.id : max), -Infinity)) + 1 //ID autogenerado en base al numero maximo de ID disponible en la BBDD
+    const ID = productos.reduce((max, el) => (el.id > max ? el.id : max), -Infinity) + 1 //ID autogenerado en base al numero maximo de ID disponible en la BBDD
     const body = req.body
-    productos.push({ id: String(ID), ...body })
+    productos.push({ ...body, id: ID })
+    fs.writeFileSync('database/products.json', JSON.stringify(productos))
     res.json({ ...productos })
 })
 
+
+//actualiza valores de un solo producto
 router.put("/:productID", (req, res) => {
     const find = productos.find((el => el.id == Number(req.params.productID)))
     const update = req.body;
 
     Object.assign(find, update)
+    fs.writeFileSync('database/products.json', JSON.stringify(productos))
     res.json({ ...productos })
 })
 
+
+//borra un producto
 router.delete("/:productID", (req, res) => {
-    productos = productos.filter(el => el.id !== Number(req.params.productID))
-    res.json({ ...productos }) 
+    productos = productos.filter(el => el.id !== Number(req.params.productID))//crea una nueva lista sin el producto que se quiere borrar
+
+    fs.writeFileSync('database/products.json', JSON.stringify(productos))
+    res.json({ ...productos })
 })
 
 export default router
