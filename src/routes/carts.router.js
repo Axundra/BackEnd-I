f64a5449f7fa1a3ab95006b70b9cd21336971015
axundra <axundra@hotmail.com>
2024-07-19 21:08:51 -0300

    retoques

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router()
 
 router.post("/", (req, res) => {
-    const ID = Date.now(),
+    const ID = Date.now(),      //genera ID en base a la fecha
         body = req.body;
     carrito.push([{ id: ID, products: [body] }])
     res.json(carrito)
@@ -11,18 +11,18 @@ router.post("/", (req, res) => {
 
 router.get("/:cartID", (req, res) => {
     const find = carrito.find((el => el.id == Number(req.params.cartID)))
-    res.json(find)
+    res.json(find.products)
 })
 
 router.post("/:cartID/product/:productID", (req, res) => {
     const Cfind = carrito.find(el => el.id == Number(req.params.cartID))   //busca el ID de compra
     if (Cfind) {//chekea si existe la orden de compra
         const prodID = Number(req.params.productID),
-            findd = Cfind.products.find(el2 => el2.productID == prodID)         //busca el producto dentro de la lista filtrada por ID para aumentar su cantidad
+            findd = Cfind.products.find(el2 => el2.productID == prodID)         //busca el producto dentro de la lista filtrada por ID para después aumentar su cantidad
 
         if (findd) {    //chekea si ya existe un producto con ese ID
             findd.cantidad++
-        } else {        //si no añade un producto
+        } else {        //si no añade un nuevo producto 
             Cfind.products.push({
                 productID: prodID,
                 cantidad: 1,
@@ -31,7 +31,7 @@ router.post("/:cartID/product/:productID", (req, res) => {
         }
 
         res.json({ carrito })
-    }else{
+    }else{//si el ID de compra no existe
         res.status(500).json({"Error": "ID de transaccion inexistente"})
     }
 })
